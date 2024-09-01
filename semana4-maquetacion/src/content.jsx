54a5cc57d54eb8b1248313aaf9385ba0121aedbb
EdwinCr <efcardenasmo@cesde.net>
2024-08-31 19:10:34 -0500

    Entrega

--- semana4-maquetacion/src/content.jsx
+++ semana4-maquetacion/src/content.jsx
@@ -2,16 +2,28 @@ const Content = () => {
     return (
         <secction className="content">
             <div className="main">
-                <h3>Sección Article</h3>
+                <p>La compañía de nuestros amigos peludos, como perros y gatos, es sumamente valiosa por diversas razones. Aquí te dejo algunas de las más destacadas:</p>
+                <ul>
+                    <li><b>Bienestar emocional:</b> Las mascotas ofrecen una fuente constante de afecto y apoyo emocional. Su presencia puede reducir el estrés, la ansiedad y la depresión, proporcionando un sentido de confort y seguridad.</li>
+                    <li><b>Compañía y lealtad:</b> Los animales de compañía son conocidos por su lealtad incondicional. Nos brindan compañía constante, lo que es especialmente importante para personas que viven solas o tienen pocas interacciones sociales.</li>
+                    <li></li>
+                </ul>
             </div>
             <div className="galery">
-                <h3>Galería de Imágenes</h3>
+                <img src="../src/images/IMG_9597.jpg" alt="" srcset="" />
+                <img src="../src/images/IMG_2217.jpg" alt="" srcset="" />
+                <img src="../src/images/IMG_1414.jpg" alt="" srcset="" />
+                <img src="../src/images/IMG_2807.jpg" alt="" srcset="" />
+                <h3>¡Respetar su vida es nuestro deber!</h3>
             </div>
             <div className="footer">
-                <h1>Todos los derechos reservados</h1>
+                <h1>Edwin Cárdenas</h1>
             </div>
             <div className="aside">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti suscipit quis ab atque vero distinctio placeat, eos odio repudiandae a inventore officia esse iure cum corporis modi. Rem, inventore reprehenderit.</p>
+                <h3>Son parte de nuestra familia</h3>
+                <img src="../src/images/IMG_7520.jpg" alt="" srcset="" />
+                <p>Su nombre era Sami... Inicialmente era de una vecina pero ella trabajaba lejos de casa por lo que siempre estaba solo, por lo cual frecuentaba mucho nuestra casa y mi mamá lo alimentaba con el tiempo empezó a quedarse hasta que nos adoptó. <br />
+                Desde ese momento ya no se fue jamás de la casa y nos acompañó hasta que una infección en la garganta lo afectó tanto que tuvimos que dormirlo   </p>
             </div>
         </secction>
 
--- semana4-maquetacion/src/header.jsx
+++ semana4-maquetacion/src/header.jsx
@@ -1,7 +1,12 @@
 const Header = () =>{
     return(
         <div className="header">
-            <h1>Hola</h1>
+            <h1>¿Qué son las mascotas para los Humanos?</h1>
+            <nav className="nav-bar">
+                <a href="#">Inicio</a>
+                <a href="#">Historia</a>
+                <a href="#">Mascotas</a>
+            </nav>
         </div>
         
     )
